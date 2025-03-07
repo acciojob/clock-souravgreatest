8ceb54724b4a6c0e44d303650c4a7b838c7da6e0
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-07 22:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let element=document.getElementById('timer');
+let curr_time=new Date();
+setInterval(
+	function(){
+		curr_time=new Date();
+		element.innerHTML=curr_time.toLocaleString();
+	},1000
+)
